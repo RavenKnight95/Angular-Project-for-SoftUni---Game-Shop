7653ac4445182a7d3f0d1cf93adb0021095c1518
Radostin Ivanov <raven_95@mail.bg>
2023-07-26 14:06:08 +0300

    updated header component

--- game-shop/src/app/core/header/header.component.ts
+++ game-shop/src/app/core/header/header.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from '../interfaces/user';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  get isLogged(): boolean {
+    return this.userService.isLogged;
+  }
+
+  getcurrentUser(): IUser {
+    return this.userService.currentUser;
+  }
+
+  constructor(private userService: UserService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  logoutHandler() {
+    this.userService.logout$().subscribe(data => console.log(data));
+    sessionStorage.removeItem('email');
+    sessionStorage.removeItem('accessToken');
+    sessionStorage.removeItem('_id');
+    this.router.navigate(['/home'])
+
+  }
 }
