16a03f824ab401dd172a2747ff5bb2d23c7c9b44
Radostin Ivanov <raven_95@mail.bg>
2023-07-26 18:12:13 +0300

    update game-list

--- game-shop/src/app/feature/games/game-list/game-list.component.ts
+++ game-shop/src/app/feature/games/game-list/game-list.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { GameService } from 'src/app/core/game.service';
+import { IGame } from 'src/app/core/interfaces/game';
+
 
 @Component({
   selector: 'app-game-list',
   templateUrl: './game-list.component.html',
   styleUrls: ['./game-list.component.css']
 })
-export class GameListComponent {
+export class GameListComponent implements OnInit {
+
+  gameList: IGame[];
+  filteredGameList: IGame[];
+
+  constructor(private gameService: GameService) { }
+
+  ngOnInit(): void {
+    this.gameService.loadGameList$().subscribe(gamelist => {
+      this.gameList = gamelist;
+    })
+  }
+  filterGames(filterForm: NgForm) {
+    let genre = filterForm.value.genre;
+
+    if (genre === 'any') {
+      this.filteredGameList = this.gameList;
+      return;
+    }
+
+    this.filteredGameList = this.gameList.filter(b => b.genre === genre);
+  }
 
 }
