3d34c5d6babddc6489430ac1b1b413e84c73c1bb
Radostin Ivanov <raven_95@mail.bg>
2023-07-26 17:13:51 +0300

    updated register css, html and ts

--- game-shop/src/app/auth/register/register.component.ts
+++ game-shop/src/app/auth/register/register.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { emailValidator, passwordMatch } from '../utils';
+import { UserService } from 'src/app/core/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  errorMessage: string;
+
+  passwordControl = new FormControl(null, [Validators.required, Validators.minLength(3)]);
+  validRepass: boolean = true;
+
+  get passwordsGroup(): FormGroup {
+    return this.registerFormGroup.controls['passwords'] as FormGroup;
+  }
+
+  registerFormGroup: FormGroup = this.formBuilder.group({
+    'email': new FormControl('', [Validators.required, emailValidator]),
+    'passwords': new FormGroup({
+      'password': this.passwordControl,
+      'repass': new FormControl(null, [passwordMatch(this.passwordControl)])
+    })
+  })
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  registerHandler(): void {
+    if (this.passwordsGroup.controls.password.value !== this.passwordsGroup.controls.repass.value) {
+      this.validRepass = false;
+      return;
+    } else {
+      this.validRepass = true;
+    }
+
+    const { email, passwords } = this.registerFormGroup.value;
+
+    const body = {
+      email: email,
+      password: passwords.password
+    };
+
+    this.userService.register$(body).subscribe({
+      next: user => {
+        this.router.navigate(['/login']);
+      }, error: (err) => {
+        this.errorMessage = err.error.message;
+      }
+    });
+  }
 
 }
