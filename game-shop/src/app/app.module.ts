c852c5ca1502ccefe4fd75b3959fb08dfca4aeff
Radostin Ivanov <raven_95@mail.bg>
2023-08-13 13:08:37 +0300

    edit

--- game-shop/src/app/app.module.ts
+++ game-shop/src/app/app.module.ts
@@ -7,10 +7,10 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
 import { HeaderComponent } from './core/header/header.component';
-import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './feature/pages/pages.module';
 import { GamesModule } from './feature/games/games.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 
 
 
